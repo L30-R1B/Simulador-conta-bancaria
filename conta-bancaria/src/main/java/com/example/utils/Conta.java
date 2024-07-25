ff155590dbdffa9f965ff90cba50f7930fcebbd0
L30-R1B <lr82460@gmail.com>
2024-07-25 11:10:39 -0300

    ..

--- conta-bancaria/src/main/java/com/example/utils/Conta.java
+++ conta-bancaria/src/main/java/com/example/utils/Conta.java
@@ -6,17 +6,24 @@ public class Conta {
     String dataAbertura = "1";
     double saldo = 0.00;
 
+    public Conta(String tipoConta, String tipoCliente, String dataAbertura, double saldo) {
+        this.tipoConta = tipoConta;
+        this.tipoCliente = tipoCliente;
+        this.dataAbertura = dataAbertura;
+        this.saldo = saldo;
+    }
+
     public void setSaldo(double novoSaldo){
-        saldo = novoSaldo;
+        this.saldo = novoSaldo;
     }
     public void setTipoConta(String tipoConta){
-        tipoConta = tipoConta;
+        this.tipoConta = tipoConta;
     }
     public void setTipoCliente(String tipoCliente){
-        tipoCliente = tipoCliente;
+        this.tipoCliente = tipoCliente;
     }
     public void setDataAbertura(String dataAbertura){
-        dataAbertura = dataAbertura;
+        this.dataAbertura = dataAbertura;
     }
     public double getSaldo(){
         return saldo;
@@ -35,6 +42,6 @@ public class Conta {
         System.out.println("Tipo de conta : "+ tipoConta);
         System.out.println("Tipo de cliente : " + tipoCliente);
         System.out.println("Data de abertura : " + dataAbertura);
-        System.out.println("Saldo" + saldo);
+        System.out.println("Saldo : " + saldo);
     }
 }
